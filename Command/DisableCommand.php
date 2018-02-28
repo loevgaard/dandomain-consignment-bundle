1430f7695c42a130238693febf026cb805567923
Joachim Løvgaard <joachim@loevgaard.dk>
2018-02-28 13:25:35 +0100

    added two commands

---
+++ Command/DisableCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Loevgaard\DandomainConsignmentBundle\Command;
+
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Loevgaard\DandomainConsignmentBundle\Exception\NonExistentManufacturerException;
+use Loevgaard\DandomainFoundation\Repository\ManufacturerRepository;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DisableCommand extends ContainerAwareCommand
+{
+    /**
+     * @var ManufacturerRepository
+     */
+    protected $manufacturerRepository;
+
+    public function __construct(ManufacturerRepository $manufacturerRepository)
+    {
+        $this->manufacturerRepository = $manufacturerRepository;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('loevgaard:dandomain-consignment:disable')
+            ->setDescription('Disables consignment for the specified manufacturer')
+            ->addArgument('manufacturer', InputArgument::REQUIRED, 'The manufacturer')
+        ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     * @throws NonExistentManufacturerException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // fetch arguments and options
+        $manufacturer = $input->getArgument('manufacturer');
+
+        // find manufacturer
+        $manufacturer = $this->manufacturerRepository->findOneByExternalId($manufacturer);
+
+        if (!$manufacturer) {
+            throw new NonExistentManufacturerException('The manufacturer does not exist');
+        }
+
+        $manufacturer->setConsignment(false);
+        $this->manufacturerRepository->flush();
+    }
+}
